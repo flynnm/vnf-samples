815f7e6b0c553e15d2b08d6ea0592bfca2e354fd
miaoyl <miaoyl@192.168.0.100>
2020-10-22 16:22:44 +0800

    update sg config

--- catalog_img_id_multinic_vsi/compute.tf
+++ catalog_img_id_multinic_vsi/compute.tf
@@ -22,45 +22,52 @@ data "ibm_is_instance_profile" "vnf_profile" {
 }
 
 ##############################################################################
+# Read/validate sg
+##############################################################################
+data "ibm_is_security_group" "vnf_security_group" {
+  id = "${var.vnf_security_group}"
+}
+
+##############################################################################
 # Create Ubuntu 18.04 virtual server.
 ##############################################################################
 
 //security group
-resource "ibm_is_security_group" "vnf_security_group" {
-  name           = "${var.vnf_security_group}"
-  vpc            = "${data.ibm_is_subnet.vnf_primary_subnet.vpc}"
-  resource_group = "${data.ibm_is_subnet.vnf_primary_subnet.resource_group}"
-}
+//resource "ibm_is_security_group" "vnf_security_group" {
+//  name           = "${var.vnf_security_group}"
+//  vpc            = "${data.ibm_is_subnet.vnf_primary_subnet.vpc}"
+//  resource_group = "${data.ibm_is_subnet.vnf_primary_subnet.resource_group}"
+//}
 
 //security group rule to allow ssh
-resource "ibm_is_security_group_rule" "vnf_sg_allow_ssh" {
-  depends_on = ["ibm_is_security_group.vnf_security_group"]
-  group     = "${ibm_is_security_group.vnf_security_group.id}"
-  direction = "inbound"
-  remote     = "0.0.0.0/0"
-  tcp {
-    port_min = 22
-    port_max = 22
-  }
-}
+//resource "ibm_is_security_group_rule" "vnf_sg_allow_ssh" {
+//  depends_on = ["ibm_is_security_group.vnf_security_group"]
+//  group     = "${ibm_is_security_group.vnf_security_group.id}"
+//  direction = "inbound"
+//  remote     = "0.0.0.0/0"
+//  tcp {
+//    port_min = 22
+//    port_max = 22
+//  }
+//}
 
 //security group rule to allow all for inbound
-resource "ibm_is_security_group_rule" "vnf_sg_rule_in_all" {
-  depends_on = ["ibm_is_security_group_rule.vnf_sg_allow_ssh"]
-  group     = "${ibm_is_security_group.vnf_security_group.id}"
-  direction = "inbound"
-  remote    = "0.0.0.0/0"
-}
+//resource "ibm_is_security_group_rule" "vnf_sg_rule_in_all" {
+//  depends_on = ["ibm_is_security_group_rule.vnf_sg_allow_ssh"]
+//  group     = "${ibm_is_security_group.vnf_security_group.id}"
+//  direction = "inbound"
+//  remote    = "0.0.0.0/0"
+//}
 
 //security group rule to allow all for outbound
-resource "ibm_is_security_group_rule" "vnf_sg_rule_out_all" {
-  depends_on = ["ibm_is_security_group_rule.vnf_sg_rule_in_all"]
-  group     = "${ibm_is_security_group.vnf_security_group.id}"
-  direction = "outbound"
-  remote    = "0.0.0.0/0"
-}
+//resource "ibm_is_security_group_rule" "vnf_sg_rule_out_all" {
+//  depends_on = ["ibm_is_security_group_rule.vnf_sg_rule_in_all"]
+//  group     = "${ibm_is_security_group.vnf_security_group.id}"
+//  direction = "outbound"
+//  remote    = "0.0.0.0/0"
+//}
 
-//vnf instance 
+//vnf instance
 resource "ibm_is_instance" "vnf_vsi" {
   depends_on = ["ibm_is_security_group_rule.vnf_sg_rule_out_all"]
   name           = "${var.vnf_instance_name}"
@@ -71,18 +78,18 @@ resource "ibm_is_instance" "vnf_vsi" {
   primary_network_interface {
     name = "eth0"
     subnet = "${data.ibm_is_subnet.vnf_primary_subnet.id}"
-    security_groups = ["${ibm_is_security_group.vnf_security_group.id}"]
+    security_groups = ["${data.ibm_is_security_group.vnf_security_group.id}"]
   }
-  
+
   dynamic "network_interfaces" {
     for_each = local.secondary_subnets
     content {
     name = format("eth%d", (network_interfaces.key+1))
     subnet = network_interfaces.value
-    security_groups = ["${ibm_is_security_group.vnf_security_group.id}"]
+    security_groups = ["${data.ibm_is_security_group.vnf_security_group.id}"]
     }
   }
-  
+
   vpc  = "${data.ibm_is_subnet.vnf_primary_subnet.vpc}"
   zone = "${data.ibm_is_subnet.vnf_primary_subnet.zone}"
   keys = ["${data.ibm_is_ssh_key.vnf_ssh_pub_key.id}"]
